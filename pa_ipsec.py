f02f3bc9a8fef211e336396d8115fdda5be05d1d
Aydin Habibov <58912919+aydinPython@users.noreply.github.com>
2023-03-21 22:00:16 +0400

    Update pa_ipsec.py

--- pa_ipsec.py
+++ pa_ipsec.py
@@ -1,11 +1,14 @@
-import paramiko
+import netmiko
+from netmiko import ConnectHandler
 import getpass # enrypt login credentials
 import time
 
 
 # define invetory for devices
 
-numberOfDev = int('How much devices ? - ')
+
+
+numberOfDev = int(input('How much devices ? - '))
 for num in range(numberOfDev):
 
     # define variables for firewall login
@@ -13,40 +16,46 @@ for num in range(numberOfDev):
     fw_user = input('Enter Firewall SSH User: ')
     fw_pass = getpass.getpass('Enter Firewall SSH Passwqord: ')
 
-    # create SSH client object
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+    fw_dev = {'ip':fw_ip,
+              'username':fw_user,
+              'password':fw_pass,
+              'device_type': 'paloalto_panos'}
+    
     # connect to firewall
-    ssh.connect(fw_ip, username=fw_user, password=fw_pass)
+    ssh_connect = ConnectHandler(**fw_dev)
+    print(f'Successful connection to {fw_ip} address')
 
+     
     # define IPsec tunnel configuration
     ike_gateway_name = input('Define IKE GW Name: ')
     ipsec_tunnel_name = input('Define IPsec Tunnel Name: ')
-    peer_public_ip = input('Peer Public IP: ')
-    pre_shared_key = input('PSK: ')
-    
+    peer_public_ip = input('Define Peer Public IP: ')
+    pre_shared_key = input('Define PSK: ')
+    vpn_zone_name = input('Define VPN Zone Name: ')
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print()
+
+
     # define Tunnel Interface number
     last_created_tunnel_interface = '28'
     tunnel_interface = input('Define Tunnel Interface Number: ')
     check = 3
     while check > 0:
-        if int(tunnel_interface) <= int(last_created_tunnel_interface):
-            print(f'The Tunnel Number {check} already exist,try highest number')
-            check=check-1
-            if check == 0:
-                break
-        elif int(tunnel_interface) > int(last_created_tunnel_interface):
-            print(f'The Tunnel Number {check} will be created')
-            check=check-1
 
+        if int(tunnel_interface) > int(last_created_tunnel_interface):
+            check=0
+        else:
+            break
+    
     # choose Phase1 Proposals
 
     IKE_Crypto_Profile_List = ['Aes256-Sha1-dh14-86400',
                               'Aes256-Sha1-dh5-86400',
                               'Aes256-Sha1-dh14-28800']
-    print(IKE_Crypto_Profile_List)
+    print('IKE-GW-Profile: ', IKE_Crypto_Profile_List)
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
     print()
+
 
     ph1_sa = ''
     phase1 = input('Choose one of them(copy and paste here): ')
@@ -57,7 +66,6 @@ for num in range(numberOfDev):
     elif phase1 == 'Aes256-Sha1-dh14-28800':
         ph1_sa = IKE_Crypto_Profile_List[2]
 
-
     # choose Phase2 Proposals
 
     IPsec_Crypto_Profile_List = ['Aes256-Sha1-dh5-86400',
@@ -65,7 +73,8 @@ for num in range(numberOfDev):
                                 'Aes256-Sha1-dh5-3600',
                                 'Aes256-Sha1-dh5-no-pfs-3600',
                                 'Aes256-Sha1-dh14-86400']
-    print(IPsec_Crypto_Profile_List)
+    print('IPsec-GW-Profile: ', IPsec_Crypto_Profile_List)
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
     print()
 
     ph2_sa = ''
@@ -81,28 +90,79 @@ for num in range(numberOfDev):
     elif phase2 == 'Aes256-Sha1-dh14-86400':
         ph2_sa = IPsec_Crypto_Profile_List[4]
     
+    print()
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print('Configuration will deploy as soon as...')
+    time.sleep(5)
+    print('Please wait ...')
 
-    # configure IKE gateway
+    # Create Tunnel Interface 
 
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} local-address ip x.y.w.z/x')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} local-address interface ae00.xywz')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} pre-shared-key ascii-text {pre_shared_key}')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol ikev1 ike-crypto-profile {ph1_sa}')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol ikev1 dpd enable yes')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol ikev1 dpd interval 10')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol ikev1 dpd retry 2')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol ikev2 dpd enable yes')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol-common nat-traversal enable no')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} protocol-common fragmentation enable no')
-    stdin, stdout, stderr = ssh.exec_command(f'set network ike gateway {ike_gateway_name} peer-address ip {peer_public_ip}')
+    command_for_Tunnel_Interface = [f'set network interface tunnel units tunnel.{tunnel_interface} ipv6 enabled no',
+                                    f'set network interface tunnel units tunnel.{tunnel_interface} ipv6 interface-id EUI-64',
+                                    f'set network interface tunnel units tunnel.{tunnel_interface} comment "Automation"',
+                                    f'set zone {vpn_zone_name} network layer3 tunnel.{tunnel_interface}',
+                                    f'set network virtual-router "default" interface tunnel.{tunnel_interface}']
 
-    # configure IPsec tunnel
-    stdin, stdout, stderr = ssh.exec_command(f'set network tunnel ipsec {ipsec_tunnel_name} auto-key ike-gateway {ike_gateway_name}')
-    stdin, stdout, stderr = ssh.exec_command(f'set network tunnel ipsec {ipsec_tunnel_name} auto-key ipsec-crypto-profile {ph2_sa)
-    stdin, stdout, stderr = ssh.exec_command(f'set network tunnel ipsec {ipsec_tunnel_name} tunnel-interface {tunnel_interface}')
+    
+    configuration_for_Tunnel_Interface = ssh_connect.send_config_set(command_for_Tunnel_Interface)
+    print(f'VPN Zone {vpn_zone_name} has been created and assigned to Virtual-Router')
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print()
+    print(f'Tunnel Interface {tunnel_interface} has been created')
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print()
 
-    # save configuration
-    stdin, stdout, stderr = ssh.exec_command('commit')
+    # Static VPN Route Configuration
+    static_route_size = int(input('How much would you like to add route? : '))
+    for route_size in range(static_route_size):
+        ip_route_name = input('Define IP Route Name: ')
+        ip_route = input('Define destination network: ')
 
-    # disconnect from firewall
-    ssh.close()
+        command_for_VPN_Route = [f'set network virtual-router "default" routing-table ip static-route {ip_route_name} path-monitor enable no',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} path-monitor failure-condition any',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} path-monitor hold-time 2',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} bfd profile None',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} interface tunnel.{tunnel_interface}',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} metric 10',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} destination {ip_route}',
+                                f'set network virtual-router "default" routing-table ip static-route {ip_route_name} route-table unicast']
+        
+        configuration_for_VPN_Route = ssh_connect.send_config_set(command_for_VPN_Route)
+        print(f'VPN Route {ip_route_name} has been created')
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print()
+
+    command_for_IKE = [f'set network ike gateway {ike_gateway_name} local-address ip 193.41.128.1/30',
+                       f'set network ike gateway {ike_gateway_name} local-address interface ethernet1/1',
+                       f'set network ike gateway {ike_gateway_name} authentication pre-shared-key key {pre_shared_key}',
+                       f'set network ike gateway {ike_gateway_name} protocol ikev1 ike-crypto-profile {ph1_sa}',
+                       f'set network ike gateway {ike_gateway_name} protocol ikev1 dpd enable yes',
+                       f'set network ike gateway {ike_gateway_name} protocol ikev1 dpd interval 10',
+                       f'set network ike gateway {ike_gateway_name} protocol ikev1 dpd retry 2',
+                       f'set network ike gateway {ike_gateway_name} protocol ikev2 dpd enable yes',
+                       f'set network ike gateway {ike_gateway_name} protocol-common nat-traversal enable no',
+                       f'set network ike gateway {ike_gateway_name} peer-address ip {peer_public_ip}',
+                       f'set network ike gateway {ike_gateway_name} protocol-common fragmentation enable no']
+        
+    configuration_for_Phase1 = ssh_connect.send_config_set(command_for_IKE)
+    print(f'IKE GW Configuration has been completed')
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print()
+
+
+    command_for_IPsec = [f'set network tunnel ipsec {ipsec_tunnel_name} auto-key ike-gateway {ike_gateway_name}',
+                         f'set network tunnel ipsec {ipsec_tunnel_name} auto-key ipsec-crypto-profile {ph2_sa}',
+                         f'set network tunnel ipsec {ipsec_tunnel_name} tunnel-monitor enable no',
+                         f'set network tunnel ipsec {ipsec_tunnel_name} anti-replay yes',
+                         f'set network tunnel ipsec {ipsec_tunnel_name} copy-tos no',
+                         f'set network tunnel ipsec {ipsec_tunnel_name} tunnel-interface tunnel.{tunnel_interface}',
+                         ]
+    configuration_for_Phase2 = ssh_connect.send_config_set(command_for_IPsec)
+    print(f'IPsec Tunnel Configuration has been completed')
+    print('#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#')
+    print()                         
+    
+    
+    ssh_connect.send_command("save")
+    ssh_connect.exit_config_mode()
